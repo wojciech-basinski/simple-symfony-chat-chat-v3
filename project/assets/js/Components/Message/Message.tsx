f462518efff62d9a54b6d20c476c078bde365d56
Wojciech Basinski <basinskiwojciech1@gmail.com>
2021-01-30 19:33:53 +0100

    Fixed bug with enter added in message when clicked without shift

--- project/assets/js/Components/Message/Message.tsx
+++ project/assets/js/Components/Message/Message.tsx
@@ -29,21 +29,30 @@ class Message extends React.Component<IProps, any> {
         return nextProps.messageText !== this.props.messageText;
     }
 
-    handleChange = (e):void => {
+    handleChange = (e): void => {
         // console.log('handle change');
         // console.log(this.textArea.current.selectionStart);
         // console.log(this.textArea.current.selectionEnd);
         this.props.handleChange(e.target.value);
     };
 
-    handleKeyPress = (e): void => {
+    // handleKeyPress = (e): void => {
+    //     if (e.which === 13 && !e.shiftKey) {
+    //         console.log('KEY UP ENTER');
+    //         e.preventDefault();
+    //         // this.props.sendMessage();
+    //         return;
+    //     }
+    //     // this.props.handleChange(this.props.messageText + e.key);
+    //     console.log(this.textArea.current.selectionStart);
+    //     console.log(this.textArea.current.selectionEnd);
+    // };
 
-        console.log('keypressed', e.which);
-        console.log(this.textArea.current.selectionStart);
-        console.log(this.textArea.current.selectionEnd);
+    handleKeyDown = (e): void => {
         if (e.which === 13 && !e.shiftKey) {
             e.preventDefault();
             this.props.sendMessage();
+            return;
         }
     };
 
@@ -66,7 +75,7 @@ class Message extends React.Component<IProps, any> {
                 <div className="row">
                     <div id="emoji-container"/>
                     <div className="col-xs-10">
-                        <textarea onClick={this.handleClick} autoFocus ref={this.textArea} className="form-control" id="message-text" cols={50} rows={2} value={this.props.messageText} onKeyUp={this.handleKeyPress} onChange={this.handleChange}/>
+                        <textarea onClick={this.handleClick} autoFocus ref={this.textArea} className="form-control" id="message-text" cols={50} rows={2} value={this.props.messageText} onKeyDown={this.handleKeyDown} /*onKeyUp={this.handleKeyPress}*/ onChange={this.handleChange}/>
                     </div>
                     <div className="col-xs-2">
                         <button className="btn btn-success btn-block" id="send" onClick={() => this.props.sendMessage()}><Trans>chat.send</Trans></button>
